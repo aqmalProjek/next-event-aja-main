dd3b94f1a813ef7e7ae39b0ffbcbf31e09778420
aqmalProjek <128565676+aqmalProjek@users.noreply.github.com>
2024-06-18 20:47:39 +0700

    Update sidenav.jsx

--- components/dashboard/sidenav.jsx
+++ components/dashboard/sidenav.jsx
@@ -25,15 +25,12 @@ export default function SideNav({ links }) {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks links={links} />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
           <button onClick={async () => {
                   await signOut()
-                  router.replace('/login');
                   }}
                   className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-violet-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <div className="hidden md:block">Log Out</div>
           </button>
-        </form>
       </div>
     </div>
   );
